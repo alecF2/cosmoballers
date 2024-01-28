0cf1be033773f787c5b30e41bc15352ee8a589bf
Alec Atienza <alec@null.net>
2024-01-27 16:35:04 -0800

    use vite

--- web-app/app/root.tsx
+++ web-app/app/root.tsx
@@ -1,17 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 export default function App() {
   return (
---
+++ web-app/vite.config.ts
@@ -0,0 +1,8 @@
+import { unstable_vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default defineConfig({
+  plugins: [remix(), tsconfigPaths(), visualizer({ emitFile: true })],
+});
