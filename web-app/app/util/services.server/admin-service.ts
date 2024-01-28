4e37b301b95cd8c1de686b3594f0d0df1ea9425d
Alec Atienza <59033593+alecF2@users.noreply.github.com>
2024-01-27 17:01:31 -0800

    feat: barebones route for admin login (#9)

--- web-app/app/util/services.server/admin-service.ts
+++ web-app/app/util/services.server/admin-service.ts
@@ -1,3 +1,5 @@
+import { eq } from 'drizzle-orm';
+import { verifyPassword } from './hash-service';
 import { logError } from './log-service';
 import { db } from '~/db.server/client';
 import { type NewAdmin, admins, type LoginAdmin } from '~/db.server/schema/admin';
@@ -8,3 +10,32 @@ export const insertAdmin = async (data: NewAdmin) => {
   return newAdmin;
 };
 
+export const loginAdmin = async (data: LoginAdmin) => {
+  const { email, password } = data;
+
+  let hash: string;
+
+  try {
+    hash = await getAdminPasswordHash(email);
+  } catch (error) {
+    // likely that email is not in database
+    logError(error);
+    return false;
+  }
+
+  // email is in database, just check if password matches
+  return verifyPassword(password, hash);
+};
+
+export const getAdminPasswordHash = async (email: string) => {
+  const [admin] = await db
+    .select({ hash: admins.password })
+    .from(admins)
+    .where(eq(admins.email, email));
+
+  if (!admin) {
+    throw new Error(`email ${email} not found`);
+  }
+
+  return admin.hash;
+};
